8c34c97221ee41df334d5db24eed671e11910f84
John Tuck <j.tuck1336@gmail.com>
2022-06-09 16:05:17 -0400

    Practice more and submit again

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /*          
+                    Create a list of video game names..
+                    Order the list of games by length of the game name.
+                    Use the lambda expression in this exercise as well.
+                  
+             */
+            var videoGames = new List<string>() { "gow2", "gow", "modernW2", "gowar3"};
+            videoGames.Sort();
+
+            foreach (var videoGame in videoGames)
+            {
+                Console.WriteLine(videoGame);
+            }
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
